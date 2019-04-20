33babaf4475c3100c81882563bae635c4e27ba86
adritake <atr_96@hotmail.com>
2019-04-20 14:18:57 +0200

    Primera version del sidebar

--- app/src/main/java/com/example/adrin/gidm/MainActivity.java
+++ app/src/main/java/com/example/adrin/gidm/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.adrin.gidm;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private String[] sectionTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        sectionTitles = getResources().getStringArray(R.array.sections_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, sectionTitles));
+        // Set the list's click listener
+       // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 }
