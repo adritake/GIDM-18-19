c075573f2c6d493fff8e5fb4a65c678d06b04f60
adritake <atr_96@hotmail.com>
2019-04-20 14:38:41 +0200

    Prueba para cambiar fragments con el sidebar

--- app/src/main/java/com/example/adrin/gidm/MainActivity.java
+++ app/src/main/java/com/example/adrin/gidm/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.adrin.gidm;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.net.Uri;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TestFragment.OnFragmentInteractionListener {
 
 
     private String[] sectionTitles;
@@ -28,6 +33,39 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, sectionTitles));
         // Set the list's click listener
-       // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            selectItem(i);
+        }
+    }
+
+    private void selectItem(int index) {
+
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new TestFragment();
+
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(index, true);
+        setTitle(sectionTitles[index]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+
     }
 }
