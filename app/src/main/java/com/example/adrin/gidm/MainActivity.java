f5023ed601f83c2abd7ce1098a2773b737c6cd4b
adritake <atr_96@hotmail.com>
2019-06-20 13:27:08 +0200

    Pongo la rutina activa como primera pantalla

--- app/src/main/java/com/example/adrin/gidm/MainActivity.java
+++ app/src/main/java/com/example/adrin/gidm/MainActivity.java
@@ -38,6 +38,11 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, new MiRutinaFragment())
+                .addToBackStack( "tag" )
+                .commit();
 
     }
 
