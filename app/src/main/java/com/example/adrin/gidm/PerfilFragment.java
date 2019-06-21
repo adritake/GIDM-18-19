d2330b9aaf84367262560d2502a8e8f66b667300
adritake <atr_96@hotmail.com>
2019-06-21 13:20:28 +0200

    Implementado edición de perfil

--- app/src/main/java/com/example/adrin/gidm/PerfilFragment.java
+++ app/src/main/java/com/example/adrin/gidm/PerfilFragment.java
@@ -1,13 +1,17 @@
 package com.example.adrin.gidm;
 
 import android.content.Context;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.adrin.data.DBSim;
 
 
@@ -72,11 +76,63 @@ public class PerfilFragment extends android.app.Fragment {
         TextView nombreText = perfilView.findViewById(R.id.nombre);
         nombreText.setText(DBSim.USER.getNombre());
 
-        TextView pesoText = perfilView.findViewById(R.id.pesoVar);
-        pesoText.setText(DBSim.USER.getPeso() + " kg");
+        final TextView pesoText = perfilView.findViewById(R.id.pesoVar);
+        pesoText.setText(String.valueOf(DBSim.USER.getPeso()));
 
-        TextView alturaText = perfilView.findViewById(R.id.alturaVar);
-        alturaText.setText(DBSim.USER.getAltura() + " cm");
+        final TextView alturaText = perfilView.findViewById(R.id.alturaVar);
+        alturaText.setText(String.valueOf(DBSim.USER.getAltura()));
+
+        final TextView textKG = perfilView.findViewById(R.id.textKG);
+        final TextView textCM = perfilView.findViewById(R.id.textCM);
+        final TextView editAltura = perfilView.findViewById(R.id.editAltura);
+        editAltura.setText(String.valueOf(DBSim.USER.getAltura()));
+        final TextView editPeso = perfilView.findViewById(R.id.editPeso);
+        editPeso.setText(String.valueOf(DBSim.USER.getPeso()));
+        final ImageButton doneEdit = perfilView.findViewById(R.id.doneEditButton);
+        final ImageButton editPerfil = perfilView.findViewById(R.id.editButton);
+
+        editPerfil.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textKG.setVisibility(View.VISIBLE);
+                textCM.setVisibility(View.VISIBLE);
+                editAltura.setVisibility(View.VISIBLE);
+                editPeso.setVisibility(View.VISIBLE);
+                doneEdit.setVisibility(View.VISIBLE);
+
+                pesoText.setVisibility(View.GONE);
+                alturaText.setVisibility(View.GONE);
+                editPerfil.setVisibility(View.GONE);
+
+
+            }
+        });
+
+        doneEdit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if(!editAltura.getText().toString().isEmpty() && !editPeso.getText().toString().isEmpty()) {
+                    DBSim.USER.setAltura(Double.parseDouble(editAltura.getText().toString()));
+                    DBSim.USER.setPeso(Double.parseDouble(editPeso.getText().toString()));
+                    pesoText.setText(editPeso.getText());
+                    alturaText.setText(editAltura.getText());
+                }
+                else{
+                    Toast.makeText(getActivity(),"Error al introducir datos", Toast.LENGTH_SHORT).show();
+                }
+
+                textKG.setVisibility(View.GONE);
+                textCM.setVisibility(View.GONE);
+                editAltura.setVisibility(View.GONE);
+                editPeso.setVisibility(View.GONE);
+                doneEdit.setVisibility(View.GONE);
+
+                pesoText.setVisibility(View.VISIBLE);
+                alturaText.setVisibility(View.VISIBLE);
+                editPerfil.setVisibility(View.VISIBLE);
+
+            }
+        });
+
 
         return perfilView;
     }
